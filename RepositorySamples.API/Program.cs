c67592b3eb6d3e2b46a4bbe8a6a81c6ec5c2c2a8
Selected Habibi <habibi@avang.ir>
2023-09-13 09:42:34 +0330

    Initial Project

---
+++ RepositorySamples.API/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using RepositorySample.DAL;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<RepSampleDBContext>(c => c.UseSqlServer("Password=h0t@@TL7X5At;User ID=ha;Initial Catalog=Repo;Data Source=db.avang.ir\\devops"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ RepositorySamples.DAL/RepSampleDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositorySamples.Domain.Categories;
+using RepositorySamples.Domain.Products;
+using RepositorySamples.Framework;
+
+namespace RepositorySample.DAL
+{
+    public class RepSampleDBContext : BaseDbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public RepSampleDBContext(DbContextOptions<RepSampleDBContext> options) : base(options)
+        {
+        }
+    }
+}
